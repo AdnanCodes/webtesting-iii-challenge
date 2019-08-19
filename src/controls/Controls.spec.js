f891105696f1ade1d143d9a745fc5a613b0282c9
Adnan <adnan.chowdhury@outlook.com>
2019-08-19 17:55:04 -0500

    Tested All logic of controls and MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -10,7 +10,7 @@ describe("Snapshot Testing <Controls />", () => {
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
-  it("should invoke a function when unlock or lock gate is clicked", () => {
+  it("should invoke a function toggle locked after button pressed", () => {
     const toggleLocked = jest.fn();
     const locked = true;
     const closed = true;
@@ -20,5 +20,25 @@ describe("Snapshot Testing <Controls />", () => {
     );
     fireEvent.click(getByText("Unlock Gate"));
     expect(toggleLocked).toHaveBeenCalled();
+  });
+  it("should invoke a function toggle close after button pressed", () => {
+    const toggleClosed = jest.fn();
+    const locked = false;
+    const closed = false;
+
+    const { getByText } = render(
+      <Controls toggleClosed={toggleClosed} locked={locked} closed={closed} />
+    );
+    fireEvent.click(getByText("Close Gate"));
+    expect(toggleClosed).toHaveBeenCalled();
+  });
+  it("Checking if the button is disabled", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+
+    expect(getByText("Open Gate").disabled).toBeTruthy();
+  });
+  it("Checking the button if disabled", () => {
+    const { getByText } = render(<Controls />);
+    expect(getByText("Lock Gate").disabled).toBeTruthy();
   });
 });
